434cbac7aae67bc20e151aac84f1f547a850a80a
akselil <69148361+akselil@users.noreply.github.com>
2020-12-07 14:43:33 +0200

    Increase version number to 3.0.0

--- lib/common/config.d.ts
+++ lib/common/config.d.ts
@@ -1,4 +1,5 @@
 import { ErrorLogConfig, GlobalLogConfig, RequestLogConfig, ResponseLogConfig } from './types';
+export declare let globalConfig: GlobalLogConfig;
 declare function getGlobalConfig(): GlobalLogConfig;
 declare function setGlobalConfig(config: GlobalLogConfig): void;
 declare function assembleBuildConfig(config?: RequestLogConfig | ResponseLogConfig | ErrorLogConfig): {
